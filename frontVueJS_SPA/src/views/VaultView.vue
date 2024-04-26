70a7142ff00fbdb5b7c7d53f15aaf92b150cdb1e
= <alexandre.roussel.48@gmail.com>
2024-04-26 14:44:03 +0200

    Add : new View for vault

---
+++ frontVueJS_SPA/src/views/VaultView.vue
@@ -0,0 +1,17 @@
+<script>
+
+export default {
+  name: 'vault'
+}
+
+</script>
+
+<template>
+  <main>
+    <div v-if="this.$store.getters.is_connected">
+      <div class="header_long">
+        <img src="@/assets/vault.svg">
+      </div>
+    </div>
+  </main>
+</template>
